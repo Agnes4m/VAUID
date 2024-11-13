e3fda170248787f1b9f8049f1d2f7b5bd763c7c3
Agnes Digital <735803792@qq.com>
2024-11-13 15:44:19 +0800

    🐛这次真修复了（

--- VAUID/va_user/add_ck.py
+++ VAUID/va_user/add_ck.py
@@ -4,7 +4,7 @@ from ..utils.database.models import VAUser
 
 
 async def add_cookie(ev: Event, uid: str, ck: str):
-    if VAUser.data_exist(user_id=ev.user_id, bot_id=ev.bot_id):
+    if not await VAUser.data_exist(user_id=ev.user_id, bot_id=ev.bot_id):
         await VAUser.insert_data(ev.user_id, ev.bot_id, cookie=ck, uid=uid)
     else:
         await VAUser.update_data(ev.user_id, ev.bot_id, cookie=ck, uid=uid)
