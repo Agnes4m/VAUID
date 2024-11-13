7e3346dcd819ae330f66ce5e32068c7ab7d93bc6
Agnes Digital <735803792@qq.com>
2024-11-13 15:33:12 +0800

    🐛补一个uid

--- VAUID/va_user/add_ck.py
+++ VAUID/va_user/add_ck.py
@@ -7,5 +7,5 @@ async def add_cookie(ev: Event, uid: str, ck: str):
     if VAUser.data_exist(user_id=ev.user_id, bot_id=ev.bot_id):
         await VAUser.insert_data(ev.user_id, ev.bot_id, cookie=ck, uid=uid)
     else:
-        await VAUser.update_data(ev.user_id, ev.bot_id, cookie=ck)
+        await VAUser.update_data(ev.user_id, ev.bot_id, cookie=ck, uid=uid)
     return '[VA]添加ck成功！'
